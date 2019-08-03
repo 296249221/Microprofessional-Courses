91bc12951af5da623be9ee6e924896ba8c5d14f4
296249221 <49741159+296249221@users.noreply.github.com>
2019-08-03 16:02:33 +0800

    第23讲笔记

--- 23被问到网络协议就懵逼？用node教你使用各大网络协议/code/wsserve.js
+++ 23被问到网络协议就懵逼？用node教你使用各大网络协议/code/wsserve.js
@@ -2,14 +2,15 @@
 const websocket = require('ws');
 const websocketServer = websocket.Server;
 const wss = new websocketServer({
-    port:3000
+    port: 3000
 })
-wss.on('connection',function(ws){
+wss.on('connection', function (ws) {
     ws.send('嘿嘿嘿，你连上我了');
-    setTimeout(function() {
+    setTimeout(function () {
         ws.send('两秒钟了，你说话啊');
     }, 2000);
-    ws.on('message',function(mes){
-        console.log()
+    ws.on('message', function (mes) {
+        console.log(mes);
+        ws.send('收到');
     })
 })
