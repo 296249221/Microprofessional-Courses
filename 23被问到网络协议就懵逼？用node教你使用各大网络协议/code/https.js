e43f4d2804d1cd181515d50d86c05a5e90d96574
zhengkai01 <zhengkai01@inspur.com>
2019-08-06 15:36:50 +0800

    第23讲笔记

--- 23被问到网络协议就懵逼？用node教你使用各大网络协议/code/https.js
+++ 23被问到网络协议就懵逼？用node教你使用各大网络协议/code/https.js
@@ -2,16 +2,16 @@
 // 生成私钥
 // openssl genrsa 1024 > ./keys/private.pem
 // 根据私钥生成公钥
-// openssl req -new -key ./keys/private.pem -out csr.pem
+// openssl req -new -key ./keys/private.pem -out ./keys/csr.pem
 // 根据公钥和私钥生成证书
-// openssl x509 -req -days 365 -in csr.pem -signkey private.pem -out file.crt
+// openssl x509 -req -days 365 -in ./keys/csr.pem -signkey ./keys/private.pem -out ./keys/file.crt
 
 const https = require('https');
 const fs = require('fs');
 var options = {
-    keys:fs.readFileSync('./keys/private.pem'),
-    cert:fs.readFileSync('./keys/file.crt')
+    key: fs.readFileSync('./keys/private.pem'),
+    cert: fs.readFileSync('./keys/file.crt')
 };
-https.createServer(options,function(req,res){
+https.createServer(options, function (req, res) {
     res.end('hello world');
 }).listen(3400);
